bb45bdb6abd758fbe43e3dfb1d04b6c02c264af1
sudarshan-supaboard <sudarshan@supaboard.cloud>
2025-03-04 14:36:46 +0530

    updated train.py

--- train.py
+++ train.py
@@ -28,8 +28,7 @@ class CustomTrainer(Trainer):
     def compute_loss(
         self, model, inputs, return_outputs=False, num_items_in_batch=None
     ):
-        print(f'num_items_in_batch: {num_items_in_batch}')
-
+        
         if isinstance(model, torch.nn.DataParallel):
             print(f"devices: {model.device_ids}")
             model = model.module
@@ -46,6 +45,8 @@ class CustomTrainer(Trainer):
         # inputs
         inputs["input_ids"] = inputs["input_ids"].to(device)
         inputs["attention_mask"] = inputs["attention_mask"].to(device)
+        
+        print(f'inputs shape: {inputs["input_ids"].shape}')
 
         mask_token_id = tokenizer.mask_token_id
         outputs = model(**inputs)
@@ -56,7 +57,6 @@ class CustomTrainer(Trainer):
         
         logits = logits[mask_token_indices[0], mask_token_indices[1], :]
 
-        # loss = F.cross_entropy(logits, labels)
         loss = criterion(logits, labels)
         
         get_memory_usage()
