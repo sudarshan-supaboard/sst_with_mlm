96fdaa7d2bc05363093ca2da69e8d9d28dbc8507
sudarshan-supaboard <sudarshan@supaboard.cloud>
2025-03-07 11:44:24 +0530

    added eval_accu_steps and empty_cache

--- train.py
+++ train.py
@@ -115,7 +115,7 @@ def train(bkt_upload=True,num_epochs=6,
         save_total_limit=4,
         report_to="wandb",
         bf16=True,
-        torch_empty_cache_steps=4,
+        torch_empty_cache_steps=10,
         eval_accumulation_steps=1,
     )
 
