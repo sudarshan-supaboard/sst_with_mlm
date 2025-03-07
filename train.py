ef1d945e14e08abdec94eca12d0dd553dabc5b73
sudarshan-supaboard <sudarshan@supaboard.cloud>
2025-03-07 12:21:00 +0530

    added eval_accu_steps

--- train.py
+++ train.py
@@ -115,8 +115,7 @@ def train(bkt_upload=True,num_epochs=6,
         save_total_limit=4,
         report_to="wandb",
         bf16=True,
-        # torch_empty_cache_steps=1,
-        eval_accumulation_steps=1,
+        eval_accumulation_steps=4,
     )
 
     es_callback = EarlyStoppingTrainingLossCallback(patience=3)
